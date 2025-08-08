f1b5702008dd40b530049b982aff8164a075a43b
moqoru <june6362@naver.com>
2025-08-08 15:39:27 +0900

    Turret 파일 주석 처리(오류 해결)

--- Engine/Unity/Example/Assets/02910000000001-EvenI/6x/E01/Practice/C6x_E01Turret_06.cs
+++ Engine/Unity/Example/Assets/02910000000001-EvenI/6x/E01/Practice/C6x_E01Turret_06.cs
@@ -1,6 +1,5 @@
 // C6x_E01Turret_06.cs 수정한 코드
 // Unity Editor에서 Inspector 뷰 => Script에서 다른 종류의 Bullet 프리팹을 넣어 준다. 
-
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -10,9 +9,8 @@ using UnityEngine.EventSystems;
 
 namespace _6x_E01Practice
 {
-	/**
-	 * 터렛
-	 */
+	/* 
+	 * 에러 방지를 위해 주석 처리 (메서드 이름 겹침)
 	public partial class C6x_E01Turret_06 : CComponent
 	{
 		#region 변수
@@ -23,33 +21,14 @@ namespace _6x_E01Practice
 		#endregion // 변수
 
 		#region 함수
-		/** 초기화 */
 		public override void Start()
 		{
 			base.Start();
-
-			/*
-			 * StartCoroutine 메서드는 코루틴을 시작하는 역할을 수행한다. (+ 즉, 
-			 * 해당 메서드를 활용하면 비동기적인 연산을 좀 더 수월하게 처리하는 것이 가능하다.)
-			 * 
-			 * 코루틴 (Coroutine) 이란?
-			 * - 프로그램의 흐름이 반환되었던 지점부터 다시 이어서 실행 할 수 있는 메서드를
-			 * 의미한다. (+ 즉, 일반적인 메서드는 프로그램의 흐름이 반환되고 나면 해당 메서드를 
-			 * 호출 시 다시 메서드의 처음부터 실행 되지만 코루틴은 반환되었던 위치부터 다시 이어서 
-			 * 실행이 가능하다는 차이점이 존재한다.)
-			 * 
-			 * 따라서 코루틴을 활용하면 여러 작업을 병렬적으로 처리하는 결과를 만들어내는 것이
-			 * 가능하다. (+ 즉, 일정 간격으로 프로그램의 흐름을 넘겨줌으로서 병렬 처리를 
-			 * 수행 할 수 있다.)
-			 */
 			StartCoroutine(this.CoTryShootBullet());
 		}
 		#endregion // 함수
 	}
 
-	/**
-	 * 터렛 - 코루틴
-	 */
 	public partial class C6x_E01Turret_06 : CComponent
 	{
 		#region 상수
@@ -58,9 +37,9 @@ namespace _6x_E01Practice
 		#endregion // 상수
 
 		#region 함수
-		/** 초기화 */
 		public IEnumerator CoTryShootBullet()
 		{
+			
 			float fInterval = Random.Range(1.0f, 3.0f);
 			var oManager_Scene = CManager_Scene.GetManager_Scene<C6x_E01Example_06>(KDefine.G_N_SCENE_EXAMPLE_06);
 
@@ -93,4 +72,6 @@ namespace _6x_E01Practice
 		}
 		#endregion // 함수
 	}
+	*/
 }
+
